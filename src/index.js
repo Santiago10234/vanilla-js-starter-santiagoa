7893ed6f5951095d441c6dc4181a50e2f3462024
santiago10234 <ariza.ps4@gmail.com>
2024-06-03 09:41:34 -0600

    Creacion del GET

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@
 let inputAgregar = document.getElementById("input-agregar").value
 let agregar = document.getElementById("agregar")
 
+async function datos() {
+    try {
+        const respuesta = await fetch("http://localhost:3000/api/task")
+        let datos = await respuesta.json()
+        console.log(datos);
+    } catch (error) {
+        console.error(error);
+    }
+}
+agregar.addEventListener("click",datos)
