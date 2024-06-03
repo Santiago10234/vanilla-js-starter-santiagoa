d5eda48e4f958729bff0ddde6db1353805215bf9
santiago10234 <ariza.ps4@gmail.com>
2024-06-03 09:59:36 -0600

    Creacion del POST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Inserte el código aquí
-let inputAgregar = document.getElementById("input-agregar").value
+let inputAgregar = document.getElementById("input-agregar")
 let agregar = document.getElementById("agregar")
 
 async function datos() {
@@ -7,6 +7,24 @@ async function datos() {
         const respuesta = await fetch("http://localhost:3000/api/task")
         let datos = await respuesta.json()
         console.log(datos);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function darDatos() {
+    try {
+        let tarea = {
+            id:Date.now(),
+            nombre:inputAgregar.value
+    }
+    const respuesta = await fetch("",{
+        method:"POST",
+        Headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body:JSON.stringify(tarea)
+    })
     } catch (error) {
         console.error(error);
     }
