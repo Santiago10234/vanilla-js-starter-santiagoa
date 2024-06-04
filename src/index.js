79df8edc7194d64c176eb1a1a5c814aade0d2721
santiago10234 <ariza.ps4@gmail.com>
2024-06-04 08:24:49 -0600

    creacion del innerHTML

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
+import { disconnect } from "process"
+
 // Inserte el código aquí
 let inputAgregar = document.getElementById("input-agregar")
+let contenedorTareas = document.getElementById("contenedorTareas")
 let agregar = document.getElementById("agregar")
 
+
 async function datos() {
+    console.log("dsad")
     try {
         const respuesta = await fetch("http://localhost:3000/api/task")
         let datos = await respuesta.json()
         console.log(datos);
+        datos.forEach(fun => {
+            let div = document.createElement("div")
+            let checkBox = document.createElement("input")
+            checkBox.type = "checkbox"
+            let p = document.createElement("p")
+            let btnElim = document.createElement("button")
+            btnElim.innerHTML="Eliminar"
+            btnElim.addEventListener("click",()=>{
+                console.log(fun.id);
+            })
+            p.innerHTML=fun.nombre
+            p.appendChild(checkBox)
+            
+            p.appendChild(btnElim)
+            div.appendChild(p)
+            contenedorTareas.appendChild(div)
+        });
     } catch (error) {
         console.error(error);
     }
+
 }
 
 async function darDatos() {
     try {
         let tarea = {
             id:Date.now(),
-            nombre:inputAgregar.value
-    }
-    const respuesta = await fetch("",{
+            nombre:inputAgregar.value,
+            estado:false
+        }
+    const respuesta = await fetch("http://localhost:3000/api/task",{
         method:"POST",
-        Headers:{
+        headers:{
             "Content-type": "application/json; charset=UTF-8"
         },
         body:JSON.stringify(tarea)
@@ -29,4 +53,10 @@ async function darDatos() {
         console.error(error);
     }
 }
-agregar.addEventListener("click",datos)
+
+agregar.addEventListener("click", function () {
+    datos()
+})
+
+
+export{datos, darDatos}
